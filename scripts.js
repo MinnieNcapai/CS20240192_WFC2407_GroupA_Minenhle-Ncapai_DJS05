b4d4e584c65c0decfcbf86d1a97c68b05e7501de
Minenhle Ncapai <minipai64@gmail.com>
2024-11-09 14:39:46 +0200

    Create reducer function to handle ADD, SUBTRACT, and RESET actions using const and arrow function

--- scripts.js
+++ scripts.js
@@ -8,3 +8,17 @@ const initialState = {
     count: 0
 };
 
+//Reducer Function
+const counterReducer  (state = initialState, action) => {
+    switch (action.type) {
+        case Add:
+            return { count: state.count + 1 };
+        case Subtract:
+            return { count: state.count - 1 };
+        case Reset:
+            return { count: 0 };
+        default:
+            return state;
+}
+}
+
